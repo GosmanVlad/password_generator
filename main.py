045f7c45b2bc2ae792c41dabda19e386fbf12fe0
Gosman Vlad Andrei <vladut.gosman@info.uaic.ro>
2021-12-06 15:30:57 +0200

    code documentation + new words in dictionary (with lowercase - first char)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import string
 import random
 import sys
-import numpy
 from sklearn.utils import shuffle
 
 max_chars = 18
@@ -20,20 +19,24 @@ numbers_list = []
 symbols_list = []
 words_list = []
 
+# Functie care citeste din fisierul dictionary.txt, linie cu linie
+# si returneaza o lista cu toate cuvintele citite
 def read_words():
     with open("dictionary.txt") as file:
         lines = file.readlines()
         lines = [line.rstrip() for line in lines]
     return lines
 
-
+# Functie care converteste un sir de caractere intr-o lista
+# Params: string - stringul din care vom obtine lista
 def string_to_list(string):
     final_list = []
     for i in range(0, len(string)):
         final_list.append(string[i])
     return final_list
 
-
+# Functie care verifica daca intr-un string sunt simboluri
+# Params: password - string-ul in care vom verifica aparitia simbolurilor
 def check_if_string_contains_symbols(password):
     for symbol in symbols_list:
         if symbol in password:
@@ -45,6 +48,9 @@ def generate_random_password(type, list):
     final_password = ""
 
     if type == "auto":
+        # Alegem o lungime random cuprinsa intre min_chars si max_chars,
+        # un simbol random si un caracter majuscula random pentru primul
+        # char din parola
         random_length = random.randint(min_chars, max_chars)
         random_symbols = symbols[random.randint(0, 3)]
         first_char = ''.join(random.choices(upper, k=1))
@@ -53,26 +59,36 @@ def generate_random_password(type, list):
         for i in range(0, random_length-1):
             final_password += list[i]
 
+        # Daca parola obtinuta nu contine niciun simbol, atunci alegem o pozitie random
+        # din parola formata pana acum si inlocuim char-ul ales cu un symbol random
         if not check_if_string_contains_symbols(final_password):
             final_password = final_password.replace(final_password[random.randint(0, len(final_password)-1)], random_symbols)
 
     if type == "dictionary":
+        # Citim din fisier fiecare cuvant, il transformam intr-un string si il amestecam
+        # Alegem o lungime random cuprinsa intre min_chars si max_chars
         words_list = read_words()
-        words_list = shuffle(words_list, random_state=3)
+        words_list = shuffle(words_list, random_state=random.randint(0, 100))
         random_length = random.randint(min_chars, max_chars)
-        random_symbols = symbols[random.randint(0, 3)]
 
         index = 0
+
+        # Parcurgem lista de cuvinte citite si verificam daca cuvantul de pe pozitia actuala
+        # incepe cu o majuscula, in caz contrar se verifica urmatorul cuvant
         for word in words_list:
             random_symbols = symbols[random.randint(0, 3)]
-            if len(final_password + word) < random_length:
-                if random.randint(0, 100) % 2 == 0 and index != 0:
-                    final_password += random_symbols
-                else:
-                    final_password += word
-            index += 1
+            if word[0].isupper():
+                if len(final_password + word) < random_length:
+                    # Avand in vedere ca in lista de cuvinte nu se regaseste niciun simbol,
+                    # alegem aleator prin aceasta conditie sa introducem un simbol, sau nu
+                    if random.randint(0, 100) % 2 == 0 and index != 0:
+                        final_password += random_symbols
+                    else:
+                        final_password += word
+                index += 1
 
-
+        # Daca combinatia dintre cuvinte formata in for-ul nu respecta lungimea aleasa
+        # se completeaza atat cu simboluri cat si cu caractere alfanumerice
         for i in range(0, random_length - len(final_password)):
             random_symbols = symbols[random.randint(0, 3)]
             if len(final_password) < random_length:
@@ -92,6 +108,8 @@ if __name__ == "__main__":
     final_password = ""
     generate_type = ""
 
+    # Convertim toate caracterele intr-o singura lista si le amestecam random
+    # pentru a obtine parola finala, respectand conditiile impuse in cerinta
     lower_list = string_to_list(lower)
     upper_list = string_to_list(upper)
     numbers_list = string_to_list(numbers)
@@ -99,6 +117,10 @@ if __name__ == "__main__":
     entire_list = merge_lists(lower_list, upper_list, numbers_list, symbols_list)
     entire_list = shuffle(entire_list, random_state=random.randint(0, 100))
 
+    # In functie de argumentul dat la rulare, algoritmul va genera parola finala
+    # tinand cont de conditiile impuse in cerinta
+    # Pentru niciun argument dat, algoritmul va creea random o parola
+    # folosindu-se de caracterele alfanumerice si de simboluri
     if len(sys.argv) > 0:
         if len(sys.argv) > 1:
             if sys.argv[1] == '-use_dict':
@@ -110,4 +132,4 @@ if __name__ == "__main__":
 
     print("Parola generata: " + final_password)
     print("Marime parola: " + str(len(final_password)))
-    print("Tip generare: " + generate_type)
+    print('Tip generare: generare automata' if generate_type == "auto" else 'Tip generare: generare utilizand dictionarul')
